8118695c226d0b54b01f32795fae5119f19d02e4
vibecode <rumashkin@gmail.com>
2017-05-12 20:38:48 +0500

    remove dummy articles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,11 +11,7 @@ export class AppComponent {
   articles: Article[];
 
   constructor() {
-    this.articles = [
-      new Article('Angular 2', 'http://angular.io', 3),
-      new Article('Fullstack', 'http://fullstack.io', 2),
-      new Article('Angular Homepage', 'http://angular.io', 1)
-    ];
+    this.articles = [];
   }
 
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
